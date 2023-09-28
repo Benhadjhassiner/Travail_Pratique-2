59b6a3cc39be5548ef13704174a5c040f9fd09ab
benhadjhassiner <benhadjhassiner@educ.college-montreal.qc.ca>
2023-09-28 12:09:32 -0400

    Jeu

--- Jeu des Devinettes.py
+++ Jeu des Devinettes.py
@@ -1,37 +1,57 @@
-#Définition des bornes
-print("Définition des limites que peut atteindre le chiffre à deviner. \n")
-petit = int(input("Le plus petit nombre (entier) peut être: \n"))
-grand = int(input("Le plus grand nombre (entier peut être: \n" ))
-
-
 #Importation du module random
 import random
-nombre = random.randint(petit, grand)
 
-#Définition de la variable pour compter le nombre d'essais
-essai = 1
+#Définition des limites que peuvent atteindre le nombre avec une variable pour le refaire facilement.
+# Les input sont les deux questions pour le minimum puis pour le maximum. L'output est le nombre aléatoire qui sera décidé
+def borne():
+    print(" \nDéfinition des limites que peut atteindre le chiffre à deviner. \n")
+    #Définition du minimum
+    global petit
+    petit = int(input("Le plus petit nombre (entier) peut être: \n"))
+    #Définition du maximum
+    global grand
+    grand = int(input("Le plus grand nombre (entier) peut être: \n"))
+    #Définition du chiffre à deviner
+    global nombre
+    nombre = random.randint(petit, grand)
+    # Définition de la variable pour compter le nombre d'essais
+    global essai
+    essai = 0
 
-guess = int(input("Que pensez-vous être le nombre (entier): \n"))
-
-if guess == nombre:
-    reussite = True
-    bravo = f"Vous avez réussi! Nombre d'essais: {essai}"
-    print(bravo)
-
-else:
-    reussite = False
+borne()
 
 
-while reussite == False:
-    if guess <= nombre:
-        print("C'est plus!")
-        guess
+#Définition de la variable qui servira en tant que condition
+jeu = True
+
+#La boucle n'opèrera que si "jeu" est True
+while jeu == True:
+    guess = int(input("À vous de deviner le nombre que j'ai choisi: \n"))
+
+    #Félicitations à l'utilisteur lorsqu'il réussi
+    if guess == nombre:
         essai += 1
-    elif guess >= nombre:
-        print("C'est moins!")
-        guess
-        essai += 1
+        bravo = f"Bravo! Bonne réponse. Vous avez réussi en: {essai} essais"
+        print(bravo)
+        #Proposition à l'utilisateur de rejouer
+        encore = str(input("Voulez-vous rejouer? 'y' pour Oui et 'n' pour Non \n"))
+        #Une nouvelle partie est commencée s'il veut rejouer
+        if encore == "y":
+            print("Recommençons!")
+            print(borne())
+        #Le jeu est terminé s'il veut quitter
+        else:
+            print("Au revoir.")
+            jeu = False
+
+    #Indices à l'utilisateur pour l'aider à deviner le nombre
     else:
-        print("Ce n'est pas un nombre entier, recommencez")
-        guess
-        essai += 1
+        #Si son nombre est trop petit
+        if guess <= nombre:
+            print("C'est plus!")
+            essai += 1
+        #Si son nombre est trop grand
+        else:
+            print("C'est moins!")
+            essai += 1
+
