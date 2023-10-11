79253686b42c9f036d35b6ebc1551840b8a91c22
benhadjhassiner <benhadjhassiner@educ.college-montreal.qc.ca>
2023-10-11 09:52:43 -0400

    Commentaires

--- Exercice 1c).py
+++ Exercice 1c).py
@@ -1,6 +1,4 @@
-# Variable que nous allons va pouvoir modifier pour choisir jusqu'à quelle position nous allons print
 nombre = 0
-# Variable de la chaîne de caractères pour être capable de la modifier dans la boucle while
 etoiles = f"***********"
 
 # Print de la chaîne de caractères en enlevant une "*" à chauqe fois jusqu'à ce qu'il n'y ai plus rien
